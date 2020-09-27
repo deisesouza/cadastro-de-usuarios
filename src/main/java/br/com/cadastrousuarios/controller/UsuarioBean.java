9b8cf1094c31d4789c290f1d373a8e5ba8e37c51
Deise Souza <deise_santana_souza@hotmail.com>
2020-09-27 08:50:38 -0300

    alteracao cadastro

--- src/main/java/br/com/cadastrousuarios/controller/UsuarioBean.java
+++ src/main/java/br/com/cadastrousuarios/controller/UsuarioBean.java
@@ -3,35 +3,50 @@ package br.com.cadastrousuarios.controller;
 import java.io.Serializable;
 
 import javax.enterprise.context.RequestScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.cadastrousuarios.dao.UsuarioDao;
 import br.com.cadastrousuarios.model.Usuario;
 
-@Named
+@Named("UsuarioBean")
 @RequestScoped
-public class UsuarioBean implements Serializable{
+public class UsuarioBean implements Serializable {
 
 	private static final long serialVersionUID = -528613798702573689L;
 
 	@Inject
 	private Usuario usuario;
 	
+	
+
 	UsuarioDao dao = new UsuarioDao();
 
-
-	public void salvar(){
+	public void salvar() {
 		try {
-			dao.salvar(usuario);	
-			FacesContext contexto = FacesContext.getCurrentInstance();
-			contexto.addMessage(null, new FacesMessage("Salvo com sucesso!"));
+			dao.salvar(usuario);
 		} catch (Exception e) {
 			e.printStackTrace();
-		}		
-		
+		}
 	}
+
+	public void remover() {
+		try {
+			dao.remover(usuario.getId());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public void consultar() {
+		try {
+			dao.listarTodos();			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
 
 }
