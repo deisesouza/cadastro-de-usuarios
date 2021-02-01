29e676946a00c3bc78e221b646d24ea65260d2d4
Deise Souza <deise_santana_souza@hotmail.com>
2021-02-01 18:35:59 +0300

    Update UsuarioBean.java

--- src/main/java/br/com/cadastrousuarios/controller/UsuarioBean.java
+++ src/main/java/br/com/cadastrousuarios/controller/UsuarioBean.java
@@ -1,28 +1,23 @@
 package br.com.cadastrousuarios.controller;
 
 import java.io.Serializable;
-
-import javax.enterprise.context.RequestScoped;
+import java.util.List;
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.cadastrousuarios.dao.UsuarioDao;
-import br.com.cadastrousuarios.model.Telefone;
 import br.com.cadastrousuarios.model.Usuario;
 import br.com.cadastrousuarios.negocio.MessageUtil;
 
 @Named("UsuarioBean")
-@RequestScoped
+@ApplicationScoped
 public class UsuarioBean implements Serializable {
 
 	private static final long serialVersionUID = -528613798702573689L;
 
 	@Inject
 	private Usuario usuario;
-	
-	@Inject
-	private Telefone telefone;
-
 	UsuarioDao dao = new UsuarioDao();
 	MessageUtil mensagens = new MessageUtil();
 
@@ -38,21 +33,26 @@ public class UsuarioBean implements Serializable {
 
 	public void remover() {
 		try {
-			dao.remover(usuario.getId());			
+			dao.remover(usuario.getId());
+			mensagens.exibirMsgSucesso();
 		} catch (Exception e) {			
+			mensagens.exibirMsgErro();	    
 			e.printStackTrace();
 		}
+	}
 
+	List<Usuario> listaUsuarios;
+	
+	public List<Usuario> listar(){
+		try {			
+			listaUsuarios = dao.listarTodos();						
+		}catch (Exception e) {
+			mensagens.exibirMsgErro();	    
+			e.printStackTrace();
+		}
+		return listaUsuarios;	
 	}
 	
-	public void consultar() {
-		try {
-			dao.listarTodos();			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
 
 	public Usuario getUsuario() {
 		return usuario;
@@ -62,15 +62,4 @@ public class UsuarioBean implements Serializable {
 		this.usuario = usuario;
 	}
 
-	public Telefone getTelefone() {
-		return telefone;
-	}
-
-	public void setTelefone(Telefone telefone) {
-		this.telefone = telefone;
-	}
-
-
-	
-	
 }
